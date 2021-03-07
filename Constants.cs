36fc590ff954acfc379dbce7805d87e5464d53d1
brassgoggles <turtleshell.au@gmail.com>
2021-03-07 11:48:20 +1000

    commit 2021_03_07

--- Constants.cs
+++ Constants.cs
@@ -7,6 +7,6 @@ namespace EquipApi
 {
     public class Constants
     {
-        public const string _CONN = "Server=tcp:equipapidbserver.database.windows.net,1433;Initial Catalog=EquipApi_db;Persist Security Info=False;User ID=equipadmin;Password=I15admin?;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public const string _CONN = "Server=tcp:equipapidbserver.database.windows.net,1433;Initial Catalog=EquipApi_db;Persist Security Info=False;User ID={id};Password={password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
